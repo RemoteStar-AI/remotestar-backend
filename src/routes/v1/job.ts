ae878a1da69ffe9d394e5f35fb238fade5fbf75f
Govind <govind.pandey@remotestar.io>
2025-04-17 01:59:01 +0530

    temp

--- src/routes/v1/job.ts
+++ src/routes/v1/job.ts
@@ -72,6 +72,7 @@ jobRouter.post("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+//test comment
 
 jobRouter.put("/", async (req, res) => {
   try {
