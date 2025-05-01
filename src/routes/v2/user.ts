449a4a09be763a6e638c98b557de7658132df4ca
Govind <govind.pandey@remotestar.io>
2025-05-02 02:06:54 +0530

    refactor: remove skill route and integrate skills fetching into user route

--- src/routes/v2/user.ts
+++ src/routes/v2/user.ts
@@ -2,16 +2,20 @@ import { Router } from "express";
 export const userRouter = Router();
 import { authenticate } from "../../middleware/firebase-auth";
 import User from "../../utils/db";
+import { Skills } from "../../utils/db";
+import { CulturalFit } from "../../utils/db";
 
 userRouter.get("/:id", authenticate, async (req, res) => {
   try {
     const id = req.params.id;
     const user = await User.findById(id);
+    const skills = await Skills.find({ userId: id });
+    const culturalFit = await CulturalFit.find({ userId: id });
     if (!user) {
       res.status(404).json({ error: "User not found" });
       return;
     } else {
-      res.status(200).json({ user });
+      res.status(200).json({ user, skills, culturalFit });
     }
   } catch (e) {
     console.error(e);
